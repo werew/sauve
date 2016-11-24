cfd6a5974cfe2bfd7b33568f90524bb4211fe249
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-24 07:30:00 +0100

    Handling files (fix needed: infinite loop caused by dirs . and ..)

--- src/scanner.c
+++ src/scanner.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 #include "common.h"
 #include "types.h"
 
@@ -66,11 +71,68 @@ char* pop_folder(){
     return folder;
 }
 
+void push_folder(char* folder){
+    PT_CHK(pthread_mutex_lock(&folders_queue.mutex))
+
+    if (list_push(folders_queue.list, folder) == -1) 
+        fail("list_push");
+
+    pthread_cond_signal(&folders_queue.pt_cond);
+    
+    PT_CHK(pthread_mutex_unlock(&folders_queue.mutex))
+}
+
+void handle_file
+(const char* basedir, const char* filename){
+    // Create path
+    size_t len_bd = strlen(basedir);
+    char* path = malloc(len_bd+strlen(filename)+1);
+    if (path == NULL) fail("malloc");
+    
+    strcpy(path,basedir);
+    path[len_bd] = '/';
+    strcpy(&path[len_bd+1],filename);
+
+    // Handle the file 
+    struct stat buf;
+    if (lstat(path, &buf) == -1) fail("lstat");
+    switch (buf.st_mode & S_IFMT) {
+        case S_IFREG: printf("%s is a regular file\n",path);
+            break;
+        case S_IFDIR: push_folder(path);
+            break;
+        case S_IFLNK: printf("%s is a link\n",path);
+            break;
+        default:  // Do not treat this file
+            free(path);
+    }
+    
+}
+
+void explore_folder(const char* folder){
+    DIR* d = opendir(folder);
+    if (d == NULL) fail("Cannot open source folder");
+    
+    struct dirent entry;
+    struct dirent* result;
+    do {
+        // TODO change name to PT_CHK
+        PT_CHK(readdir_r(d, &entry, &result));
+        
+        handle_file(folder, entry.d_name);
+
+    } while (result != NULL);
+     
+    if (closedir(d) == -1) fail("closedir");
+}
+
 void* scanner(void* arg){
     // Get a folder
     char* folder;
     while ((folder = pop_folder()) != NULL){
         printf("%s\n",folder);
+        explore_folder(folder);
+        free(folder);
     }
     signal_term();    
     return (void*) 0; 
