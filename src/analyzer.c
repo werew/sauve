0bcc3ae6ca01f4ab361921c62f298bc10636bf4c
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-28 01:24:39 +0100

    Copy file

--- src/analyzer.c
+++ src/analyzer.c
@@ -5,8 +5,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <utime.h>
 #include "analyzer.h"
 #include "common.h"
 #include "types.h"
@@ -30,11 +32,47 @@ char* pop_file(){
 }
 
 
+void copy_file(const char* file){
+
+    // Build the path to the copy
+    char* copy = change_base(file, SENV.source, SENV.destination); 
+    if (copy == NULL) fail("change_base"); 
+
+    // Get some infos about the file to copy
+    struct stat buf;
+    if (lstat(file, &buf) == -1) fail("lstat");
+    mode_t perms = buf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+
+    // Open the source and the destination
+    // (...setting the good permissions)
+    int fd_src, fd_dst;
+    if ((fd_src = open(file, O_RDONLY)) == -1 || 
+        (fd_dst = open(copy, O_WRONLY|O_CREAT, perms)) == -1) 
+        fail("open");
+
+    // Main copy loop
+    ssize_t n;
+    char copy_buf[1024]; 
+    while ((n = read(fd_src,copy_buf,1024)) != 0){
+        if (write(fd_dst, copy_buf, n) < n) fail("write");
+    }
+
+    if (close(fd_src) == -1 || 
+        close(fd_dst) == -1 ) fail("close");
+
+    // Set the good access and modification time    
+    struct utimbuf times = { buf.st_atime, buf.st_mtime };
+    if (utime(copy,&times) == -1) fail("utime");
+    
+    free(copy);
+}
+
 void* analyzer(void* arg){
     // Get a file
     char* file;
     while ((file = pop_file()) != NULL){
         printf("\t\t--file--> %s\n",file);
+        copy_file(file);
         free(file);
     }
     signal_term();    
